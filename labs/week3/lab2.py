f1203edfecad65dce6c13d3c7d39c3fa984208f0
Andry Bintoro <bintoro@ab-mac.local>
2018-02-13 10:07:35 -0500

    adding week3 lab files

---
+++ labs/week3/lab2.py
@@ -0,0 +1,85 @@
+"""
+lab2.py
+Andry Bintoro
+1/30/2018
+"""
+
+def squared_nums(num_list):
+    """
+    squares numbers in num_list
+    num_list: list of numbers
+    Returns: list of these numbers squared
+    """
+    new_list = [ ]  #initialize list to hold results
+    #iterate through num_list and square each element
+    for num in num_list:
+        sq_num = pow(num,2)
+        new_list.append(sq_num)
+    return new_list
+
+def check_title(title_list):
+
+    """
+    Removes strings in title_list that have numbers and aren't title case
+    title_list: list of strings
+    Returns: list of strings that are titles
+    """
+    #initialize list to hold strings
+    title_list = [" "]
+    #iterate through title_list and remove strings that have numbers
+    for string in title_list:
+        if string.istitle() and string.isalpha():
+            title_list.append(string)
+    return title_list
+
+def restock_inventory(inventory):
+    """
+    Increases inventory of each item in dictionary by 10
+    inventory: a dictionary with:
+        key: string that is the name of the inventory item
+        value: integer that equals the number of that item currently on hand
+    Returns: updated dictionary where each inventory item is restocked
+    """
+    #initialize new inventory
+    inventory = { }
+
+    for key, value in inventory.items():
+        val = val + 10
+        inventory[key] = val
+    return inventory
+
+def filter_0_items(inventory):
+    """
+    Removes items that have a value of 0 from a dictionary of inventories
+    inventory: dictionary with:
+        key: tring that is the name of the inventory item
+        value: nteger that equals the number of that item currently on hand
+    Returns: the same inventory_dict with any item that had 0 quantity removed
+    """
+    #initialize new inventory
+    inventory = { }
+
+    for key, value in inventory.items():
+        if value in inventory == 0:
+            inventory[key]=value
+        """
+        if value == 0:
+            inventory.pop({key:None})
+        else:
+            inventory.update()
+        """
+    return inventory
+
+def average_grades(grades):
+    """
+    Takes grade values from a dictionary and averages them into a final grade
+        grades: a dictionary of grades with:
+        key: string of students name
+        value: list of integer grades received in class
+    Returns: dictionary that averages out the grades of each student
+    """
+    #initialize new grades
+    new_grades = {}
+    for name, grades in grades.items():
+        new_grades[name]=sum(grades) /len(grades)
+    return new_grades
---
+++ labs/week3/lab3.py
@@ -0,0 +1,131 @@
+"""
+lab3.py
+Andry Bintoro
+2/6/2018
+"""
+
+def switch_case(str_list):
+    """
+    Maps strings in the str_list to a new string of
+    same characters, but the first letter contains
+    the opposite case
+    str_list: list of strings
+    Returns: list of original strings, but with
+    opposite casing
+    """
+    if str_list[0][:1].isupper():
+        str_list[0][:1].swapcase()
+    elif str_list[0][:1].islower():
+        str_list[0][:1].swapcase()
+    else:
+        pass
+
+    return str_list
+
+def only_even(mixed_list):
+    """
+    Filters out odd integers and strings that
+    contain an odd number of characters.
+    mixed_list: list of integers and/or strings
+    Returns: list of only integers and strings that
+    are even or have an even number of characters.
+    """
+    
+def greatest_difference(num_list):
+    """
+    Finds the maximum and minimum numbers in a_list
+    and computes the difference.
+    num_list: list of numbers
+    Returns: the difference between the maximum and
+    minimum numbers in num_list
+    """
+
+def make_title(words):
+    """
+    Maps words in a list to words in the same list,
+    but as titled strings.
+    words: list of words
+    Returns: new list of titled words
+    """
+
+def test_title(names):
+    """
+    Filters out capitalized and non- cap words into
+    their respective lists.
+    names: list of names
+    Returns: both lists for review
+    """
+
+def create_word(char_list):
+    """
+    Takes a list of characters and creates a word
+    (list with alpha chars only) from them.
+    letters: list of etters
+    Returns: list that has alpha characters only
+    """
+
+def three_times_nums(num_list):
+    """
+    Maps numbers in the num_list to numbers that are
+    3 times the original value
+    num_list: list of numbers
+    Returns: list of numbers that are of three times
+    the values in num_list
+    """
+
+def keep_lowercase(str_list):
+    """
+    Filters out strings that have uppercase values
+    str_list: list of strings
+    Returns: list of strings that do not contain
+    uppercase values
+    """
+
+def multiplication_total_of(num_list):
+    """
+    Multiplies all the numbers in num_list together
+    and gives the total
+    num_list: list of numbers
+    Returns: the multiplied total of the numbers in
+    the num_list
+    """
+
+def square_nums(number_list):
+    """
+    Maps numbers in the number_list to numbers of
+    same value, but squares the number given
+    number_list: list of numbers
+    Returns: list from number_list which are squared
+    """
+
+def lessthan_5(num_list):
+    """
+    Filters out numbers less than five
+    num_list: list of numbers
+    Returns: list of numbers in the original list
+    that are less than five
+    """
+
+def subtraction_of(number_list):
+    """
+    Subtracts the numbers in number_list
+    number_list: list of numbers
+    Returns: the difference of the numbers in the
+    number_list
+    """
+
+def double_nums(num_list):
+    """
+    Maps numbers in the num_list to their doubles
+    num_list: list of numbers
+    Returns: list of doubled numbers
+    """
+
+def remove_special_characters(string_list):
+    """
+    Filters out strings that have non-alphanumeric
+    elements
+    char_list: list of strings
+    Returns: list of strings that have only letters
+    or numbers in them
+    """
